24306dec851b7035e11718f3ff844b50ff5398c4
Jo Phillips <joxphillips@gmail.com>
2018-04-13 17:13:59 +0000

    added main file

---
+++ MathBattle.java
@@ -0,0 +1,36 @@
+
+
+import java.utils.Scanner;
+
+public class MathBattle
+{
+	public static void main(String[] args)
+	{
+	int playerNumber =0;
+	Scanner in = new Scanner(System.in);
+	System.out.println("enter 1 for single player mode. Enter 2 for two player mode: ");
+	playerNumber = in.nextInt();
+
+	if(playerNumber ==1)
+		{
+
+
+		} else if (playerNumber ==2)
+		{
+
+
+		}else
+		{
+
+		}
+
+
+	}
+
+
+
+
+
+
+
+}
