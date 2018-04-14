e8683d769bc3fd1e6d7d36b561061297eecb0fcf
Jo Phillips <joxphillips@gmail.com>
2018-04-14 16:47:25 +0000

    added the view class. right now all it has is a place holder for the second streen

---
+++ Interface.java
@@ -0,0 +1,27 @@
+
+import java.awt.event.*;
+
+public class Interface extends JFrame implements ACtionListener
+{
+private String name;
+private int state;
+	public Interface(String pName);
+	{
+	name = pName;
+
+	}
+	public void FirstScreen(Equation Equation1, Equation Equation2, Equation Equation3)
+	{
+	JPanel screen2 = new JPanel();
+	screen2.setLayout(new FlowLayout());
+
+
+	}
+
+
+
+
+
+
+
+}
