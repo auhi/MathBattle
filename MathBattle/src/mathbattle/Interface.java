a30645a1db525a2f85b4a8f82c32dfac92f674c3
JO <joxphillips@gmail.com>
2018-04-14 18:10:49 -0600

    had to make a bunch of changes with the class to get it to work.

---
+++ MathBattle/src/mathbattle/Interface.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathbattle;
+
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author JO Phillips
+ */
+public class Interface
+{
+	private String name;
+	private int state;
+
+	public Interface(String name, int state)
+	{
+		this.name = name;
+		this.state = state;
+	}
+public void FirstScreen()
+	{
+	
+	JPanel screen2 = new JPanel();
+	screen2.setLayout(new FlowLayout());
+	screen2.add(new JLabel() );
+		
+	
+	}
+
+
+}
