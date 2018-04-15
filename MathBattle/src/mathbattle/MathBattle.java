93e0666df89de54f0c603bf55ae06a037460d107
JO <joxphillips@gmail.com>
2018-04-14 18:12:19 -0600

    made new file

---
+++ MathBattle/src/mathbattle/MathBattle.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathbattle;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JO Phillips
+ */
+public class MathBattle
+{
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+	
+	int playerNumber =0;
+	Scanner in = new Scanner(System.in);
+	System.out.println("enter 1 for single player mode. Enter 2 for two player mode: ");
+	playerNumber = in.nextInt();
+
+	if(playerNumber ==1)
+		{
+
+
+		} else if (playerNumber ==2)
+		{
+
+
+		}else
+		{
+
+		}
+
+
+	}
+
+
+}
+
+
