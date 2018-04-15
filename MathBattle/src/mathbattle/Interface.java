113e508f0bf2ead9c20fff0630b9c520a2515cd9
JO <joxphillips@gmail.com>
2018-04-14 19:08:28 -0600

    Got the 2nd view window to work, just has place holder text for now.

--- MathBattle/src/mathbattle/Interface.java
+++ MathBattle/src/mathbattle/Interface.java
@@ -6,32 +6,41 @@
 package mathbattle;
 
 import java.awt.FlowLayout;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
  * @author JO Phillips
  */
-public class Interface
+public class Interface  
 {
+
 	private String name;
 	private int state;
 
-	public Interface(String name, int state)
+	Interface(int playerNumber, String playerName)
 	{
-		this.name = name;
-		this.state = state;
-	}
-public void FirstScreen()
-	{
-	
-	JPanel screen2 = new JPanel();
-	screen2.setLayout(new FlowLayout());
-	screen2.add(new JLabel() );
-		
-	
+		state = playerNumber;
+		name = playerName;
+
 	}
 
+	public void FirstScreen() 
+	{
+		
+		JFrame FirstFrame = new JFrame();
+		JPanel screen2 = new JPanel();
+		screen2.setLayout(new FlowLayout());
+		screen2.add(new JLabel("placeholder text"));
+		screen2.add(new JTextField(10));
+		FirstFrame.setVisible(true);
+		FirstFrame.setContentPane(screen2);
+		FirstFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FirstFrame.setTitle(name);
+		FirstFrame.pack();
+	}
 
 }
